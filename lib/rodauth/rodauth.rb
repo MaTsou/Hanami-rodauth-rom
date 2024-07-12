2d836c9b9194bdba76531060221b48137515676a
MaTsou <mazu@sfr.fr>
2024-07-12 10:09:58 +0200

    Add remember Rodauth feature.

--- lib/rodauth/rodauth.rb
+++ lib/rodauth/rodauth.rb
@@ -8,7 +8,7 @@ class RodauthAuthentication < Roda
   # do sessions have to be managed by hanami ?
   plugin :sessions, secret: "Mylong secret string which has to be more than 64 char long and long and long"
   plugin :rodauth do
-    enable :login, :logout, :create_account, :reset_password
+    enable :login, :logout, :create_account, :reset_password, :remember
     #password_minimum_length 10
     # role may be registered in a membership table together with foreign_keys 
     # user_id and company_id
@@ -19,14 +19,19 @@ class RodauthAuthentication < Roda
     login_return_to_requested_location? true
     reset_password_autologin? true
     logout_redirect "/login"
+
+    after_create_account do
+      remember_login
+    end
   end
   plugin :render, escape: true, views: 'lib/rodauth/'
 
   route do |r|
+    rodauth.load_memory # auto-login if remembered account..
     r.rodauth
 
-    #env['rodauth'] = rodauth
+    #env['rodauth'] = rodauth # if rodauth needed through middleware stack..
     # below, redirect to login page unless authenticated...
-    rodauth.require_authentication
+    rodauth.require_account
   end
 end
