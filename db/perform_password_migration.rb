02f4b7ffd16a2dc1c1ba2804ef4e3c5d2c3e98ff
MaTsou <mazu@sfr.fr>
2024-07-14 11:14:50 +0200

    Now password specific migration takes advantage of DATABASE_URL env var.

--- db/perform_password_migration.rb
+++ db/perform_password_migration.rb
@@ -1,10 +1,26 @@
 require 'sequel'
+if ENV['DATABASE_URL']
+  # DATABASE_URL env var format is
+  # protocole://user_name:pwd@host
+  #
+  # providing a dedicated regex, one can get each required parts of it :
+  # user_name, pwd and host as first part, second part and third part.
+  parts = ENV['DATABASE_URL'].match /[^\/]*\/\/([^:]*):([^@]*)@(.*)/
 
-Sequel.extension :migration
-Sequel.postgres( 'sane_budget', user: 'sane_budget_password' ) do |db|
-  Sequel::Migrator.run(
-    db,
-    "#{File.dirname(__FILE__)}/rodauth_pw_migrate",
-    table: 'schema_info_password'
-  )
+  Sequel.extension :migration
+  Sequel.postgres(
+    parts[1],
+    user: "#{parts[1]}_password", # here the pwd specific user
+    password: parts[2],
+    host: parts[3]
+  ) do |db|
+    Sequel::Migrator.run(
+      db,
+      "#{File.dirname(__FILE__)}/rodauth_pw_migrate",
+      table: 'schema_info_password'
+    )
+  end
+
+else
+  p "please provide a DATABASE_URL env var.."
 end
