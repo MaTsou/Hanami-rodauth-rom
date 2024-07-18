ac00b2a489094a15a3733ec661a3deace48789e2
MaTsou <mazu@sfr.fr>
2024-07-18 17:55:38 +0200

    Add a post_repo repository.

---
+++ app/repositories/post_repo.rb
@@ -0,0 +1,19 @@
+module SaneBudget
+  module Repositories
+    class PostRepo < Repository[:posts]
+      commands :create, update: :by_pk, delete: :by_pk
+
+      def by_id( id )
+        posts.by_pk( id ).one
+      end
+
+      def from_account( id )
+        posts.with_account_id account_id
+        # or directly posts.combine( :account ).where( ... ) but this last way 
+        # bind repo with db column name ( account_id ! ). Repo have to be db 
+        # agnostic !
+      end
+
+    end
+  end
+end
