cfbb923143c300199a407912cf2c5facd771039b
MaTsou <mazu@sfr.fr>
2024-07-18 17:51:17 +0200

    Add posts relation and update account relation with has_many posts.

--- lib/sane_budget/persistence/relations/posts.rb
+++ lib/sane_budget/persistence/relations/posts.rb
@@ -1,16 +1,20 @@
 module SaneBudget
   module Persistence
     module Relations
-      class Accounts < ROM::Relation[:sql]
-        schema :accounts, infer: true do
+      class Posts < ROM::Relation[:sql]
+        schema :posts, infer: true do
           associations do
-            has_many :posts
+            belongs_to :account
           end
         end
 
         auto_struct true # or use Entities (see repository.rb)
         # I think Entities are redundant with Views::Parts... and are a 
         # exposition responsability so Parts are designed for this.
+
+        def with_account_id( id )
+          combine( :account ).where( account_id: id ) # can't use 
+        end
       end
     end
   end
