04d7844563d4f39d5b57be5774e6d8de870f6ae3
MaTsou <mazu@sfr.fr>
2024-07-18 17:35:54 +0200

    Add migration files to create blogs table and timestamps triggers.

---
+++ db/migrate/20240716145639_create_blog.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+ROM::SQL.migration do
+  change do
+    create_table( :blogs ) do
+      primary_key :id, type: :Bignum
+      foreign_key :account_id, :accounts, null: false, type: :Bignum, on_delete: :cascade
+      String :title, null: false
+      String :body
+      DateTime :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      DateTime :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      index :account_id
+    end
+  end
+end
