dd012d583da49e8efc2a522c67fae7c43a967ac2
MaTsou <mazu@sfr.fr>
2024-07-18 18:01:05 +0200

    Configure Hanami app to manage session and cookies.

--- config/app.rb
+++ config/app.rb
@@ -4,5 +4,15 @@ require "hanami"
 
 module SaneBudget
   class App < Hanami::App
+    # hanami manage session for form submission (csrf token)
+    # rodauth manage session for authentication (account_id value)
+    config.actions.sessions = :cookie, {
+      key: "sane_budget_hanami_session",
+      secret: settings.session_secret,
+      expire_after: 60*60*24*365
+    }
+    config.actions.cookies = {
+      domain: "sane_budget.logicore.fr"
+    }
   end
 end
