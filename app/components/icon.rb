0a04e76ee2e73da3ef7bb6a4f57bfe43cc969ad9
MaTsou <mazu@sfr.fr>
2024-07-30 10:27:16 +0200

    Refactor icon.rb component class. Add a 'define' method returning a hash of icons.

--- app/components/icon.rb
+++ app/components/icon.rb
@@ -1,45 +1,49 @@
 module SaneBudget
   module Components
     class Icon
-      def initialize( **options )
-        @options = {
-          style: "vertical-align: top;"
-        }.merge options
+      def initialize
+        @options = { style: "vertical-align: top;" }
       end
 
-      def render( name, **options )
+      def render( name, *options )
         %Q(
-          <iconify-icon icon="mdi:#{name}" #{to_string( compute( options ) )}>
+          <iconify-icon icon='mdi:#{name}' #{to_string( compute( *options ) )}>
           </iconify-icon>
         ).html_safe
+      end
+
+      def define( icons, **options )
+        icons.merge( icons ) do |k, v|
+          render( k, options, v )
+        end
       end
 
       private
 
       def to_string( options )
         %Q(
-          style="#{options[:style]}"
-          class="#{options[:class]}"
-        )
+          style='#{options[:style]}'
+          class='#{options[:class]}'
+        ).gsub("\n", "").gsub(/\s\s+/," ").strip
       end
 
-      def compute( options )
+      def compute( *options )
         {
-          style: styles( @options, options).join('; '),
-          class: classes( @options, options ).join(' ')
+          style: styles( @options, *options).join('; '),
+          class: classes( @options, *options ).join(' ')
         }
       end
 
       def styles( *options )
         options.map do |opt|
           opt.fetch( :style, '' ).split(';').map(&:strip)
-        end
+        end.flatten
       end
 
       def classes( *options )
         options.map do |opt|
-          opt.fetch( :class, '' )
-        end
+          opt.fetch( :class, '' ).split(' ').map(&:strip)
+        end.flatten
       end
     end
   end
